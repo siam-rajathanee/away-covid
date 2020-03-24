003adad6db1f32bde43d5d9a1d2d05723872b41c
aegistz <teerayoot5056@gmail.com>
2020-03-24 17:19:50 +0700

    sdf

--- web_service/get_point_volunteer.php
+++ web_service/get_point_volunteer.php
@@ -15,50 +15,17 @@ pg_query("SET client_encoding = 'utf-8'");
 
 
 
-$sql = "select *,ST_AsGeoJSON(geom) AS geojson from volunteer
+$sql = "select *
+from volunteer
 WHERE status_request = 'ร้องขอ'  ; ";
 
-      
+$result = pg_query( $sql);
+$coursesArray = array();
 
-
-$query = pg_query($db,$sql);   
-$geojson = array(
-   'type'      => 'FeatureCollection',
-   'features'  => array()
-); 
-
-while($edge=pg_fetch_assoc($query)) {  
-   $feature = array(
-      'type' => 'Feature',
-      'geometry' => json_decode($edge['geojson'], true),
-      'crs' => array(
-         'type' => 'EPSG',
-         'properties' => array('code' => '4326')
-      ),
-         'properties' => array(
-         'gid' => $edge['gid'],
-         'place_name' => $edge['place_name'],
-         'lat' => $edge['lat'],
-         'lon' => $edge['lon'],
-         'case_numbe' => $edge['case_numbe'],
-         'date_start' => $edge['date_start'],
-         'status_new' => $edge['status_new'],
-         'status_pat' => $edge['status_pat'],
-         'descriptio' => $edge['descriptio'],
-         'ref_source' => $edge['ref_source'],
-         'link_news' => $edge['link_news'],
-         'tb_code' => $edge['tb_code'],
-         'tb_th' => $edge['tb_th'],
-         'ap_th' => $edge['ap_th'],
-         'pro_th' => $edge['pro_th'],
-         'postcode' => $edge['postcode'],
-         'age' => $edge['age'],
-         'gender' => $edge['gender']
-      )
-   );   
-   array_push($geojson['features'], $feature);
+while ($row = pg_fetch_assoc($result)) {
+$coursesArray[] = $row;
 }
-echo json_encode($geojson);
+echo json_encode($coursesArray);
 
 
 
