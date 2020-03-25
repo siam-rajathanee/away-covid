ae7c9097aedf873e1f75e1d40bcf7f96a898c5d9
aegistz <teerayoot5056@gmail.com>
2020-03-25 12:20:30 +0700

    ss

--- route.js
+++ route.js
@@ -1,123 +1,91 @@
 $(document).ready(async function () {
     await liff.init({ liffId: "1653984157-0qam36em" })
-    loadMap();
 });
 
 var map = L.map('map', {
     center: [16.820378, 100.265787],
     zoom: 13
 });
+var Stamen = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+    subdomains: 'abcd',
+    maxZoom: 20
+}).addTo(map)
 
 var urlParams = new URLSearchParams(window.location.search);
 var marker, gps, dataurl, tam, amp, pro, x, y;
 var url = 'https://rti2dss.com:3200';
-// var url = 'http://localhost:3100';
-
-function loadMap() {
-    var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 20,
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    });
-    var Stamen = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
-        subdomains: 'abcd',
-        maxZoom: 20
-    });
-    const grod = L.tileLayer('https://{s}.google.com/vt/lyrs=r&x={x}&y={y}&z={z}', {
-        maxZoom: 20,
-        subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
-    });
-    const ghyb = L.tileLayer('https://{s}.google.com/vt/lyrs=y,m&x={x}&y={y}&z={z}', {
-        maxZoom: 20,
-        subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
-    });
-    var pro = L.tileLayer.wms("http://rti2dss.com:8080/geoserver/th/wms?", {
-        layers: 'th:province_4326',
-        format: 'image/png',
-        transparent: true
-    });
-    var baseMap = {
-        "OSM": osm,
-        "แผนที่ขาวดำ": Stamen.addTo(map),
-        "แผนที่ถนน": grod,
-        "แผนที่ภาพถ่าย": ghyb
-    }
-}
-
-var place;
-async function onLocationFound(e) {
-    x = e.latlng.lat;
-    y = e.latlng.lng;
-    gps = await L.marker(e.latlng, { draggable: true });
-}
-
-function onLocationError(e) {
-    console.log(e.message);
-}
-
-function refreshPage() {
-    location.reload(true);
-}
-
-map.on('locationfound', onLocationFound);
-// map.on('locationerror', onLocationError);
-// map.locate({ setView: true, maxZoom: 18 });
-
-var lc = L.control.locate({
-    position: 'topleft',
-    strings: {
-        title: "Show me where I am, yo!"
-    },
-    locateOptions: {
-        enableHighAccuracy: true,
-    }
-}).addTo(map);
-
-lc.start();
 
 
-function checkStatus() {
-    console.log(gps._latlng)
-    if (!gps._latlng) {
-        alert('ไม่พบพิกัดของท่าน กรุณาเปิด gps แล้วลองใหม่อีกครั้ง')
-    } else {
-        loadData();
-    }
-}
 
-// copy from nui
-let covidlab = L.layerGroup().addTo(map);
-function loadData() {
-    $.ajax({
-        url: url + '/anticov-api/labcovid',
-        method: 'get',
-        success: function (res) {
-            const items = res.data;
+function onLocationFound(e) {
 
-            const icon = 'https://github.com/mapedia-th/away-covid/blob/master/img/hospital.png?raw=true';
-            const iconMarker = L.icon({
-                iconUrl: icon,
-                iconSize: [50, 50],
-            });
+    var radius = 150;
+    latlng = [e.latlng.lng, e.latlng.lat]
 
+    var point = turf.point(latlng);
 
-            items.forEach(item => {
-                let mk = L.marker([Number(item.lat), Number(item.long)], {
-                    icon: iconMarker
-                }).bindPopup(
-                    '<br/><span >สถานที่: </span>' + item.name +
-                    '<br/><span >ลิ้งค์: </span><a href="https://www.google.com/maps/dir/' + gps._latlng.lat + ',' + gps._latlng.lng + '/' + Number(item.lat) + ',' + Number(item.long) + '/data=!3m1!4b1!4m2!4m1!3e0">เส้นทาง</a>'
-                );
-
-                mk.addTo(covidlab);
-
-                $('#select_place').append($('<option>', {
-                    value: item.lat + ',' + item.long,
-                    text: item.name
-                }));
+    L.geoJson(point, {
+        pointToLayer: function (feature, latlng) {
+            return L.marker(latlng, {
+                icon: local_icon,
+                highlight: 'permanent'
             });
         }
     })
+        .bindPopup("ตำแหน่งปัจจุบันของท่าน")
+        .addTo(map)
+
+    var buffered = turf.buffer(point, radius, { units: 'kilometers' });
+
+    var buffereds = L.geoJson(buffered, {
+        stroke: false,
+        color: 'green',
+        fillColor: 'green',
+        fillOpacity: 0.1,
+    }).addTo(map)
+
+    map.fitBounds(buffereds.getBounds())
+
 }
+
+map.on('locationfound', onLocationFound);
+map.locate();
+
+
+var local_icon = L.icon({
+    iconUrl: 'https://mapedia-th.github.io/away-covid/img/icon.png',
+    iconSize: [20, 20]
+});
+
+
+let covidlab = L.layerGroup().addTo(map);
+$.getJSON("https://rti2dss.com:3200/anticov-api/labcovid", function (res) {
+    const items = res.data;
+
+    const icon = 'https://github.com/mapedia-th/away-covid/blob/master/img/hospital.png?raw=true';
+    const iconMarker = L.icon({
+        iconUrl: icon,
+        iconSize: [50, 50],
+    });
+
+    items.forEach(item => {
+        let mk = L.marker([Number(item.lat), Number(item.long)], {
+            icon: iconMarker
+        }).bindPopup(
+            '<br/><span >สถานที่: </span>' + item.name +
+            '<br/><span >ลิ้งค์: </span><a href="https://www.google.com/maps/dir/' + latlng[1] + ',' + latlng[0] + '/' + Number(item.lat) + ',' + Number(item.long) + '/data=!3m1!4b1!4m2!4m1!3e0">เส้นทาง</a>'
+        );
+
+        mk.addTo(covidlab);
+
+        $('#select_place').append($('<option>', {
+            value: item.lat + ',' + item.long,
+            text: item.name
+        }));
+    });
+
+})
+
 
 
 
