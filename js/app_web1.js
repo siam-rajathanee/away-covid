b5e9cde39c80a8fe469ec7b7d6743ae33b40fb64
aegistz <teerayoot5056@gmail.com>
2020-03-26 10:38:59 +0700

    dd

--- js/app_web1.js
+++ js/app_web1.js
@@ -1,45 +1,39 @@
+// async function getUserProfile() {
+//     profile = await liff.getProfile()
+//     pictureUrl = profile.pictureUrl
+//     userId = profile.userId
+//     displayName = profile.displayName
+//     decodedIDToken = liff.getDecodedIDToken().email
+//     if (pictureUrl == undefined) {
+//         pictureUrl = ''
+//     }
 
-async function getUserProfile() {
-    profile = await liff.getProfile()
-    pictureUrl = profile.pictureUrl
-    userId = profile.userId
-    displayName = profile.displayName
-    decodedIDToken = liff.getDecodedIDToken().email
-    if (pictureUrl == undefined) {
-        pictureUrl = ''
-    }
+//     $.ajax({
+//         url: 'https://rti2dss.com/mapedia.serv/add_tracking.php?type=login',
+//         method: 'post',
+//         data: ({
+//             pictureUrl: pictureUrl,
+//             userId: userId,
+//             displayName: displayName,
+//             decodedIDToken: decodedIDToken,
+//             page_view: 'index.html'
+//         }),
+//         success: function (data) {
+//         }
+//     })
 
-    $.ajax({
-        url: 'https://rti2dss.com/mapedia.serv/add_tracking.php?type=login',
-        method: 'post',
-        data: ({
-            pictureUrl: pictureUrl,
-            userId: userId,
-            displayName: displayName,
-            decodedIDToken: decodedIDToken,
-            page_view: 'index.html'
-        }),
-        success: function (data) {
-
-        }
-    })
-
-}
-async function main() {
-    liff.ready.then(() => {
-        if (liff.isLoggedIn()) {
-            getUserProfile()
-
-        } else {
-            liff.login()
-        }
-    })
-    await liff.init({ liffId: "1653981898-q0jEx1on" })
-}
-main()
-
-
-
+// }
+// async function main() {
+//     liff.ready.then(() => {
+//         if (liff.isLoggedIn()) {
+//             getUserProfile()
+//         } else {
+//             liff.login()
+//         }
+//     })
+//     await liff.init({ liffId: "1653981898-q0jEx1on" })
+// }
+// main()
 
 
 new Vue({
