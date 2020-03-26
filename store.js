a1158bbbc4470da9dfbb24e74fde848db731dfc1
sakda <sakda.homhuan@gmail.com>
2020-03-26 21:24:46 +0700

    test

--- store.js
+++ store.js
@@ -1,40 +1,40 @@
-async function getUserProfile() {
-    profile = await liff.getProfile()
-    pictureUrl = profile.pictureUrl
-    userId = profile.userId
-    displayName = profile.displayName
-    decodedIDToken = liff.getDecodedIDToken().email
-    if (pictureUrl == undefined) {
-        pictureUrl = ''
-    }
+// async function getUserProfile() {
+//     profile = await liff.getProfile()
+//     pictureUrl = profile.pictureUrl
+//     userId = profile.userId
+//     displayName = profile.displayName
+//     decodedIDToken = liff.getDecodedIDToken().email
+//     if (pictureUrl == undefined) {
+//         pictureUrl = ''
+//     }
 
-    $.ajax({
-        url: 'https://rti2dss.com/mapedia.serv/add_tracking.php?type=login',
-        method: 'post',
-        data: ({
-            pictureUrl: pictureUrl,
-            userId: userId,
-            displayName: displayName,
-            decodedIDToken: decodedIDToken,
-            page_view: 'store.html'
-        }),
-        success: function (data) {
-        }
-    })
+//     $.ajax({
+//         url: 'https://rti2dss.com/mapedia.serv/add_tracking.php?type=login',
+//         method: 'post',
+//         data: ({
+//             pictureUrl: pictureUrl,
+//             userId: userId,
+//             displayName: displayName,
+//             decodedIDToken: decodedIDToken,
+//             page_view: 'store.html'
+//         }),
+//         success: function (data) {
+//         }
+//     })
 
-}
-async function main() {
-    liff.ready.then(() => {
-        if (liff.isLoggedIn()) {
-            getUserProfile()
-        } else {
-            liff.login()
-        }
-    })
-    // await liff.init({ liffId: "1653981898-NPG2rwM9" })
-    await liff.init({ liffId: "1653984157-Yn4O7eAO" })
-}
-main()
+// }
+// async function main() {
+//     liff.ready.then(() => {
+//         if (liff.isLoggedIn()) {
+//             getUserProfile()
+//         } else {
+//             liff.login()
+//         }
+//     })
+//     // await liff.init({ liffId: "1653981898-NPG2rwM9" })
+//     await liff.init({ liffId: "1653981898-NPG2rwM9" })
+// }
+// main()
 
 
 
@@ -42,13 +42,13 @@ main()
 let userid;
 $(document).ready(async function () {
     // 1653984157-Yn4O7eAO //ของ rti2dss 1653981898-NPG2rwM9
-    await liff.init({ liffId: "1653984157-Yn4O7eAO" }, (e) => {
+    await liff.init({ liffId: "1653981898-NPG2rwM9" }, (e) => {
         // alert('1: ' + e)
     }, err => console.error(err.code, error.message));
 
     const profile = await liff.getProfile();
     userid = await profile.userId;
-    // alert('2: ' + userid)
+    alert('2: ' + userid)
     await loadMap();
     await getStore();
 });
