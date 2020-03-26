32f7f0e188f994094c597b0aa8970163b4276f45
Chingchai Humhong <chingchai.h@gmail.com>
2020-03-26 10:56:15 +0700

    Update store.js

--- store.js
+++ store.js
@@ -22,13 +22,13 @@ var url = 'https://rti2dss.com:3200';
 // var url = 'http://localhost:3200';
 
 function loadMap() {
-    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-        maxZoom: 18,
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-            '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-            'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        id: 'mapbox/light-v9',
-        tileSize: 512,
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+        maxZoom: 19,
+        // attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+        //     '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+        //     'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        // id: 'mapbox/light-v9',
+        // tileSize: 512,
         zoomOffset: -1
     }).addTo(map);
 }
