1da62d46393cc2e640a522506b699dbdbaf4cbf4
aegistz <teerayoot5056@gmail.com>
2020-03-28 16:34:19 +0700

    Update app_web1.js

--- js/app_web1.js
+++ js/app_web1.js
@@ -7,7 +7,11 @@ async function getUserProfile() {
     if (pictureUrl == undefined) {
         pictureUrl = ''
     }
+    console.log(pictureUrl);
+    console.log('<img id="img_profile" class="profile_img" src="' + pictureUrl + '" alt="">');
 
+    document.getElementById('displayname').innerHTML = '<h4 id="displayname">' + displayName + '</h4>'
+    document.getElementById('img_profile').innerHTML = '<img id="img_profile" class="profile_img" src="' + pictureUrl + '" alt="">'
     $.ajax({
         url: 'https://rti2dss.com/mapedia.serv/add_tracking.php?type=login',
         method: 'post',
@@ -21,11 +25,7 @@ async function getUserProfile() {
         success: function (data) {
         }
     })
-    console.log(pictureUrl);
-    console.log('<img id="img_profile" class="profile_img" src="' + pictureUrl + '" alt="">');
 
-    document.getElementById('displayname').innerHTML = '<h4 id="displayname">' + displayName + '</h4>'
-    document.getElementById('img_profile').innerHTML = '<img id="img_profile" class="profile_img" src="' + pictureUrl + '" alt="">'
 }
 async function main() {
     liff.ready.then(() => {
