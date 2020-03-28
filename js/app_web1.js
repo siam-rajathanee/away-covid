a7345535f4dce31d0ab4a53ccc6349188be2beb4
aegistz <teerayoot5056@gmail.com>
2020-03-28 16:37:27 +0700

    d

--- js/app_web1.js
+++ js/app_web1.js
@@ -66,8 +66,6 @@ new Vue({
                     layer.bindPopup(popup)
                 }
 
-                console.log(pictureUrl);
-                console.log('<img id="img_profile" class="profile_img" src="' + pictureUrl + '" alt="">');
                 document.getElementById('displayname').innerHTML = '<h4 id="displayname">' + displayName + '</h4>'
                 document.getElementById('img_profile').innerHTML = '<img id="img_profile" class="profile_img" src="' + pictureUrl + '" alt="">'
 
