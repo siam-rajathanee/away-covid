b4dd2ac76509d31bd97965e809651f2524e38669
aegistz <teerayoot5056@gmail.com>
2020-04-10 14:57:23 +0700

    ddd

--- route.js
+++ route.js
@@ -9,7 +9,6 @@ async function getUserProfile() {
     }
     document.getElementById('displayname').innerHTML = '<h5 id="displayname">' + displayName + '</h5>'
     document.getElementById('img_profile').innerHTML = '<img id="img_profile" class="profile_img" src="' + pictureUrl + '" alt="">'
-
     $.ajax({
         url: 'https://mapedia.co.th/demo/add_tracking.php?type=login',
         method: 'post',
