a990edbedc23a9414bad3d7e804a41f136a0f6f1
Chingchai Humhong <chingchai.h@gmail.com>
2020-04-13 11:52:54 +0700

    Update app_web1.js

--- js/app_web1.js
+++ js/app_web1.js
@@ -64,10 +64,10 @@ CartoDB_DarkMatter = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}
     maxZoom: 19
 })
 
-stadia = L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png', {
-    maxZoom: 20,
-    attributions: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
-})
+// stadia = L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png', {
+//     maxZoom: 20,
+//     attributions: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+// })
 
 osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attributions: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -750,22 +750,22 @@ $("#form_setting").submit(function (event) {
     if (basemap == 'base1') {
         gmap.addTo(map)
         osm.remove()
-        stadia.remove()
+        CartoDB_Positron.remove()
         CartoDB_DarkMatter.remove()
     } else if (basemap == 'base2') {
         gmap.remove()
         osm.addTo(map)
-        stadia.remove()
+        CartoDB_Positron.remove()
         CartoDB_DarkMatter.remove()
     } else if (basemap == 'base3') {
         gmap.remove()
         osm.remove()
-        stadia.addTo(map)
+        CartoDB_Positron.addTo(map)
         CartoDB_DarkMatter.remove()
     } else {
-        hmap.remove()
         gmap.remove()
         osm.remove()
+        CartoDB_Positron.remove()
         CartoDB_DarkMatter.addTo(map)
     }
 
